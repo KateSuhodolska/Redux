eac69b08807dde40a08fafcfac921fa0577ae67b
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-10-11 11:19:34 +0300

    lessons

--- L1/C1/src/App.jsx
+++ L1/C1/src/App.jsx
@@ -20,7 +20,9 @@ class App extends Component {
         <ThemeContext.Provider value={this.state.theme}>
           <ThemedButton onClick={this.toggleTheme}>Dynamic Theme</ThemedButton>
         </ThemeContext.Provider>
-        <ThemedButton onClick={this.toggleTheme}>Default Theme</ThemedButton>
+        <ThemeContext.Provider>
+          <ThemedButton onClick={this.toggleTheme}>Default Theme</ThemedButton>
+        </ThemeContext.Provider>
       </div>
     );
   }
