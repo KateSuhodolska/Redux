67dc0d17966ae623a115fec8bfde53a465a37a57
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-10-12 11:25:12 +0300

    lessons

---
+++ L2/C1/src/index.js
@@ -0,0 +1,28 @@
+import { createStore } from "redux";
+
+export const increment = () => {
+    return {
+        type: "COUNTER/INCREMENT",
+    };
+};
+
+export const decrement = () => {
+    return {
+        type: "COUNTER/DECREMENT",
+    };
+};
+
+export const counterReducer = (state = 0, action) => {
+    switch (action.type) {
+        case "COUNTER/INCREMENT":
+            return state + 1;
+        case "COUNTER/DECREMENT":
+            return state - 1;
+        default:
+            return state;
+    }
+};
+
+const store = createStore(counterReducer);
+
+console.log(store.getState());
