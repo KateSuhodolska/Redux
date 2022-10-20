fdff0a3d69c2006acde1fa697a7c1f019580d473
Ekaterina Suhodolskaya <90890687+KateSuhodolska@users.noreply.github.com>
2022-10-20 10:53:46 +0300

    lessons

---
+++ L4/H1/src/users/UsersList.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+import { connect } from "react-redux";
+import { goToNextPage, goToPrevPage } from "../users.actions";
+import Pagination from "./Pagination";
+import User from "./User";
+
+const UsersList = ({ users, goToNextPage, goToPrevPage }) => {
+  const itemsPerPage = 3;
+  const { usersList, currentPage } = users;
+  const startIndex = currentPage * itemsPerPage;
+  return (
+    <div>
+      <Pagination
+        goPrev={goToPrevPage}
+        goNext={goToNextPage}
+        currentPage={currentPage}
+        totalItems={usersList.length}
+        itemsPerPage={itemsPerPage}
+      />
+      <ul className="users">
+        {usersList.slice(startIndex, startIndex + itemsPerPage).map((user) => (
+          <User key={user.id} name={user.name} age={user.age} />
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+const mapState = (state) => {
+  return {
+    users: state,
+  };
+};
+
+const mapDispatch = {
+  goToNextPage,
+  goToPrevPage,
+};
+
+const connector = connect(mapState, mapDispatch);
+
+export default connector(UsersList);
